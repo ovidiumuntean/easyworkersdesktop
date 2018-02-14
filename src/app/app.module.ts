162a936efa7942af687beb852fcce8d5260689d8
ovidiumuntean2006@gmail.com <Microsoft1875!>
2018-02-14 18:39:34 +0000

    2nd comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,25 @@ import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
+import { EmployeeRegisterComponent } from './components/employee-register/employee-register.component';
+import { CompanyRegisterComponent } from './components/company-register/company-register.component';
+import { CompanyHomeComponent } from './components/company-home/company-home.component';
+import { CompanyProfileComponent } from './components/company-profile/company-profile.component';
+import { CompanyLoginComponent } from './components/company-login/company-login.component';
+import { CreateJobComponent } from './components/create-job/create-job.component';
+import { ViewCompanyJobsComponent } from './components/view-company-jobs/view-company-jobs.component';
 
 const appRoutes: Routes =  [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
+  {path:'employeeRegister', component: EmployeeRegisterComponent},
+  {path:'company/register', component: CompanyRegisterComponent},
+  {path:'company/login', component: CompanyLoginComponent},
+  {path:'company/profile', component: CompanyProfileComponent, canActivate:[AuthGuard]},
+  {path:'company/createJob', component: CreateJobComponent},
+  {path:'company/jobs', component: ViewCompanyJobsComponent},
+  {path:'company', component: CompanyHomeComponent},
   {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
   {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]}
 ]
@@ -33,7 +47,14 @@ const appRoutes: Routes =  [
     HomeComponent,
     LoginComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    EmployeeRegisterComponent,
+    CompanyRegisterComponent,
+    CompanyHomeComponent,
+    CompanyProfileComponent,
+    CompanyLoginComponent,
+    CreateJobComponent,
+    ViewCompanyJobsComponent
   ],
   imports: [
     BrowserModule,
